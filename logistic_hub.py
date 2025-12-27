545b27afe011f031af6a7a5ff63c8aaf0ac76b2a
Oskar Sałata <ixpleew@Oskars-MacBook-Air.local>
2025-12-27 17:31:51 +0100

    Imported libraries for graphs and showing them on charts

--- logistic_hub.py
+++ logistic_hub.py
@@ -1,3 +1,20 @@
+import warehouses
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
 class LogisticHub:
     def __init__(self):
         self._warehouse_lists = []
+
+
+warehouse_graph = nx.Graph()
+first = warehouses.Warehouse("Warsaw", 2000)
+second = warehouses.Warehouse("Cracow", 1500)
+
+warehouse_graph.add_node(first)
+warehouse_graph.add_node(second)
+
+warehouse_graph.add_edge(first, second)
+nx.draw(warehouse_graph)
+plt.show()
