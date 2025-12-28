f2c1c2c85f9b7ead31c18cf82beac699ddeaf8c5
Oskar Sałata <ixpleew@Oskars-MacBook-Air.local>
2025-12-28 14:34:06 +0100

    Fixed bug with adding edges to the graph

--- logistic_hub.py
+++ logistic_hub.py
@@ -1,6 +1,6 @@
 import warehouses
 import networkx as nx
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import json
 
 
@@ -8,25 +8,29 @@ class LogisticHub:
     def __init__(self):
         with open("data.json", mode="r") as file:
             data = json.load(file)
-        self.ware_graph = nx.Graph()
+        self._ware_graph = nx.Graph()
         for warehouse_data in data["warehouses"]:
             warehouse = warehouses.Warehouse(
                 warehouse_data["name"],
                 warehouse_data["max_capacity"],
                 warehouse_data["current_capacity"])
-            self.ware_graph.add_node(warehouse_data["name"], item=warehouse)
+            self._ware_graph.add_node(warehouse_data["name"], item=warehouse)
 
         for source in data["warehouses"]:
             for destination in source["connections"]:
                 target = destination["target_name"]
                 distance = destination["distance"]
 
-                self.ware_graph.add_edge(
-                    source,
+                self._ware_graph.add_edge(
+                    source["name"],
                     target,
                     weight=distance
                 )
 
-# warehouse_graph.add_edge(first, second)
-# nx.draw(warehouse_graph)
-# plt.show()
+    def graph(self):
+        return self._ware_graph
+
+
+logisticHub = LogisticHub()
+nx.draw(logisticHub.graph())
+plt.show()
--- warehouses.py
+++ warehouses.py
@@ -1,6 +1,6 @@
 class Warehouse:
     def __init__(self, name, max_capacity, curr_capacity, connections=None):
-        self._name = name
+        self._name = name   
         self._max_capacity = max_capacity
         if curr_capacity > max_capacity:
             raise ValueError("Warehouse overloaded!")
