ebac2461302950a72d1c13c37695122171d6edce
Oskar Sałata <ixpleew@Oskars-MacBook-Air.local>
2025-12-28 17:43:06 +0100

    Added list to LogisticHub to have data about all of the warehouses

--- logistic_hub.py
+++ logistic_hub.py
@@ -6,15 +6,17 @@ import json
 
 class LogisticHub:
     def __init__(self):
+        self._ware_graph = nx.Graph()
+        self._ware_list = []
         with open("data.json", mode="r") as file:
             data = json.load(file)
-        self._ware_graph = nx.Graph()
         for warehouse_data in data["warehouses"]:
             warehouse = warehouses.Warehouse(
                 warehouse_data["name"],
                 warehouse_data["max_capacity"],
                 warehouse_data["current_capacity"])
             self._ware_graph.add_node(warehouse_data["name"], item=warehouse)
+            self._ware_list.append(warehouse)
 
         for source in data["warehouses"]:
             for destination in source["connections"]:
