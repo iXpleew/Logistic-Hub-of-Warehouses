55d493293b67a1495b0fcc63ed63fe4ec13dbfcd
Oskar Sałata <ixpleew@Oskars-MacBook-Air.local>
2025-12-29 14:34:34 +0100

    Added a condition to check if given capacity is larger than maximum capacity and giving an error if true

--- warehouses.py
+++ warehouses.py
@@ -2,5 +2,11 @@ class Warehouse:
     def __init__(self, name, max_capaci, curr_capaci=None, connections=None):
         self._name = name
         self._max_capaci = max_capaci
+        if curr_capaci is not None:
+            quantity_sum = 0
+            for product in curr_capaci:
+                quantity_sum += product["product_quantity"]
+            if quantity_sum > max_capaci:
+                raise ValueError("Current capacity larger than maximux")
         self._curr_capaci = curr_capaci
         self._connections = connections
