d06609a365fa8ec3d70951be8c7ef2c318af9613
Oskar Sałata <ixpleew@UniFi-62-85.elka.pw.edu.pl>
2026-01-08 12:11:11 +0100

    Started to work on a menu

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ test_data = "testing_data.json"
 
 if __name__ == "__main__":
     hub = LogisticHub(start_data)
-    hub.show_what_warehouse_has("Warsaw Hub")
-    hub.add_product("Warsaw Hub", "Carrot", 3000)
-    hub.show_what_warehouse_has("Warsaw Hub")
-    hub.start_request("Warsaw Hub", "Poznan Annex", "Carrot", 10)
-    hub.show_actual_requests()
-    hub.draw_graph()
+    print("--------------------------")
+    print("-------Logistic Hub-------")
+    print("--------------------------")
+    print("-What do you want to do?--")
+    print()
+    print("1. See warehouses on a graph")
+    print("2. Add product to a warehouse")
+    print("3. Remove product from a warehouse")
+    print("4. Look for a specific product across hub")
+    print("5. Look what specific warehouse has")
+    print("6. Start request")
+    print("7. Save hub to file")
+    print("Other options: Exit")
+    choice = input()
 
-    # remember to reduce number of product that are sent to warehouse
-    # when request has
+    match choice:
+        case "1":
+            hub.draw_graph()
+        case _:
+            print("Thanks for using that programme!")
